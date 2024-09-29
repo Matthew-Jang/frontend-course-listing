b4a2f80a4d32802d57b189d4c4ee0ef907a9064d
matthew-jang <matthew.jang@eagles.oc.edu>
2024-09-29 15:41:50 -0500

    initial update page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ViewCourses from '../views/ViewCourses.vue'
 import AboutView from '../views/AboutView.vue' // Import the AboutView component
+import UpdateCourse from '../views/UpdateCourse.vue' // Import the AboutView component
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/courses',
       name: 'courses',
       component: ViewCourses
+    },
+    {
+      path: '/courses/update',
+      name: 'updateCourse',
+      component: UpdateCourse
     }
   ]
 })
---
+++ src/views/UpdateCourse.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+  <v-container>
+    <h1>Update Course</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Department</th>
+          <th>Course Number</th>
+          <th>Level</th>
+          <th>Hours</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!--:key="course.id"-->
+        <tr>
+          <td>{{ course.dept }}</td>
+          <td>{{ course.course_number }}</td>
+          <td>{{ course.Level }}</td>
+          <td>{{ course.Hours }}</td>
+          <td>{{ course.Name }}</td>
+          <td>{{ course.Description }}</td>
+          <td>
+            <div class="button-group">
+              <!-- Update button -->
+              <v-btn color="green" @click="updateCourse(course.id)">Update</v-btn>
+              <!-- Delete button -->
+              <v-btn color="red" @click="deleteCourse(course.id)">Delete</v-btn>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </v-container>
+</template>
+
+<style scoped></style>
