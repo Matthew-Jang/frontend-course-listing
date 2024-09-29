58e4df8c50be6d44b42aa3e56dfccaa13658b7da
matthew-jang <matthew.jang@eagles.oc.edu>
2024-09-29 13:16:18 -0500

    adding add course page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 import ViewCourses from '../views/ViewCourses.vue'
-import AboutView from '../views/AboutView.vue' // Import the AboutView component
+import AddCourse from '../views/AddCourse.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/courses',
       name: 'courses',
       component: ViewCourses
+    },
+    {
+      path: '/add-course',
+      name: 'add',
+      component: AddCourse
     }
   ]
 })
---
+++ src/views/AddCourse.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { addCourse } from '../services/courseService.js'
+</script>
+
+<template>
+  <v-container class="d-flex justify-center align-center" style="height: 100vh">
+    <v-form>
+      <v-row>
+        <!-- Text Field -->
+        <v-col cols="12">
+          <v-text-field v-model="deptField" label="Enter Dept" outlined clearable></v-text-field>
+        </v-col>
+
+        <!-- Text Field -->
+        <v-col cols="12">
+          <v-text-field
+            v-model="courseNumberField"
+            label="Enter course number"
+            outlined
+            clearable
+          ></v-text-field>
+        </v-col>
+
+        <!-- Text Field -->
+        <v-col cols="12">
+          <v-text-field v-model="levelField" label="Enter level" outlined clearable></v-text-field>
+        </v-col>
+
+        <!-- Text Field -->
+        <v-col cols="12">
+          <v-text-field v-model="hoursField" label="Enter hours" outlined clearable></v-text-field>
+        </v-col>
+
+        <!-- Text Field -->
+        <v-col cols="12">
+          <v-text-field v-model="nameField" label="Enter name" outlined clearable></v-text-field>
+        </v-col>
+
+        <!-- Text Field -->
+        <v-col cols="12">
+          <v-text-field
+            v-model="descriptionField"
+            label="Enter description"
+            outlined
+            clearable
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <!-- Button Section -->
+      <v-row class="mt-4" justify="space-between">
+        <!-- Cancel Button -->
+        <v-btn color="error" @click="onCancel"> Cancel </v-btn>
+        <!-- Save Button -->
+        <v-btn color="primary" @click="onSave"> Save </v-btn>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      deptField: '',
+      courseNumberField: '',
+      levelField: '',
+      hoursField: '',
+      nameField: '',
+      descriptionField: ''
+    }
+  },
+  methods: {
+    async onSave() {
+      try {
+        const newCourse = {
+          dept: this.deptField,
+          course_number: this.courseNumberField,
+          Level: this.levelField,
+          Hours: this.hoursField,
+          Name: this.nameField,
+          Description: this.descriptionField
+        }
+        await addCourse(newCourse)
+        this.$router.push('/courses')
+      } catch (error) {
+        console.error('Error saving course:', error)
+        alert('Failed to save course. Please try again.')
+      }
+    },
+    onCancel() {
+      this.$router.push('/courses')
+    }
+  }
+}
+</script>
