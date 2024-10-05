ff7f2a9d7515d625fba8833bf6abc9c747d95ba8
matthew-jang <matthew.jang@eagles.oc.edu>
2024-10-05 01:49:57 -0500

    test using ip address for api

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3014/api'
+  baseURL: 'http://project2.eaglesoftwareteam.com/api/course-t4'
 })
 
 // const api = axios.create({
-//     baseURL: 'http://localhost:3014/course-t4'
-//    })
+//   baseURL: 'http://localhost:3014/api/course-t4'
+// })
 
-export const getCourses = () => api.get('/course-t4')
-export const getCourseById = (id) => api.get(`/course-t4/${id}`)
-export const addCourse = (course) => api.post('/course-t4', course)
-export const updateCourse = (id, course) => api.put(`/course-t4/${id}`, course)
-export const deleteCourse = (id) => api.delete(`/course-t4/${id}`)
+export const getCourses = () => api.get('/')
+export const getCourseById = (id) => api.get(`/${id}`)
+export const addCourse = (course) => api.post('/', course)
+export const updateCourse = (id, course) => api.put(`/${id}`, course)
+export const deleteCourse = (id) => api.delete(`/${id}`)
