aa6eb1d8b8fd14636efdb8b055977814bc65d765
matthew-jang <matthew.jang@eagles.oc.edu>
2024-10-05 21:28:45 -0500

    test

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://project2.eaglesoftwareteam.com/api/course-t4'
+  baseURL: 'http://project2.eaglesoftwareteam.com:3014/api/course-t4'
 })
 
 // const api = axios.create({
